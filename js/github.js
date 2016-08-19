570c19bc78cba93d92d7797aa8922e94a1188951
Russ Davies <russdavies392@gmail.com>
2016-08-19 12:52:15 -0700

    add ability to display a user's email when entered

--- js/github.js
+++ js/github.js
@@ -5,9 +5,9 @@ function GitHub(){
 
 GitHub.prototype.getRepos = function(username) {
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
+    $('.showUser').text("the email of " + username + " is " + response.email);
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    $('.showUser').text(error.responseJSON.message);
   });
 };
 
