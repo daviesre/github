b0d6170595a1c71cdd952a8e0e69fc67ac4d7780
Russ Davies <russdavies392@gmail.com>
2016-08-19 13:12:30 -0700

    display login, email, and bio information

--- js/github.js
+++ js/github.js
@@ -5,7 +5,8 @@ function GitHub(){
 
 GitHub.prototype.getRepos = function(username) {
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    $('.showUser').text("the email of " + username + " is " + response.email);
+    $('.showUser').html("<p>Login: " + response.login + "</p><p>Email: " + response.email + "</p><p>Description: " + response.bio + "</p>");
+    console.log(response);
   }).fail(function(error){
     $('.showUser').text(error.responseJSON.message);
   });
