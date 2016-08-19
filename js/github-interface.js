36c8434f30f9c6aa9aa69ad18ae0ddac3a861035
Russ Davies <russdavies392@gmail.com>
2016-08-19 12:07:01 -0700

    add jquery and connection to apikey

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,1 +1,16 @@
+var apiKey = require('./../.env').apiKey;
 
+$(document).ready(function() {
+  $("#username-find").click(function() {
+    console.log("hello");
+    var username = $("#username").val();
+    $("#username").val("");
+    exports.getRepos = function(){
+      $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+      console.log(response);
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
+    };
+  });
+});
