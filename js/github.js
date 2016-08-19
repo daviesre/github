630370ccab02c0c831503ac6cec7fb9f45850eed
Russ Davies <russdavies392@gmail.com>
2016-08-19 13:53:51 -0700

    retrieves repo info and prints out name, desc, and url

--- js/github.js
+++ js/github.js
@@ -4,9 +4,15 @@ function GitHub(){
 }
 
 GitHub.prototype.getRepos = function(username) {
-  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    $('.showUser').html("<p>Login: " + response.login + "</p><p>Email: " + response.email + "</p><p>Description: " + response.bio + "</p>");
-    console.log(response);
+  $('.showUser').text("");
+  $.get('https://api.github.com/users/' + username + '/repos?type?access_token=' + apiKey).then(function(response){
+    for (var i = 0; i < response.length; i++) {
+      $('.showUser').append("<li><strong>Repository</strong>: " + response[i].name + "</li>");
+      $('.showUser').append("<li>Description: " + response[i].description + "</li>");
+      $('.showUser').append("<li>URL: " + response[i].url + "</li>");
+      $('.showUser').append(
+      console.log(response);
+    }
   }).fail(function(error){
     $('.showUser').text(error.responseJSON.message);
   });
